cf3c5a233e37f6dd6e18e2ab1d7defd4d7b8c0bd
Tomás Mollinedo <mollinedotomas@gmail.com>
2025-09-18 14:53:20 -0300

    primera versión

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Encabezado from './Componentes/Encabezado'
+import SubidorImagenes from './Componentes/SubidorImagenes'
+
+function App() {
+
+  return (
+    <>
+			<Encabezado/>
+      <SubidorImagenes/>
+		</>
+  )
+}
+
+export default App
---
+++ src/Componentes/Encabezado.jsx
@@ -0,0 +1,9 @@
+function Encabezado (){
+	return (
+		<>
+		<h1>Tomás Mollinedo</h1>
+        <h2>45.774.449</h2>
+		</>
+	);
+}
+export default Encabezado;
